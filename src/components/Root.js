99b9b539519d8f010f844abb30c18dabdd998e57
Andrew Landis <andrew.landis3@gmail.com>
2020-09-21 21:22:15 -0400

    Initial get and loading added

--- src/components/Root.js
+++ src/components/Root.js
@@ -5,13 +5,30 @@ class Root extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      count: null,
       loading: false
     };
   }
 
+  // Get initial count when page loads
+  componentDidMount() {
+    if (this.state.count == null) {
+      fetch(
+        'https://api.countapi.xyz/get/1ccb732e-b55a-4404-ad3f-0f99c02fe44e',
+        {
+          method: 'GET'
+        }
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        const value = data.value;
+        this.setState(() => ({ count: value }))
+      })
+    }
+  }
+ 
+  // When button is clicked, GET from hit endpoint and update state
   onHitClick = () => {
-
     fetch(
       'https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e',
       {
@@ -25,10 +42,11 @@ class Root extends React.Component {
     })
   }
 
-  
+  // Multiple Counters can be rendered here
   render() {
     return (
       <div>
+        {this.state.count == null && <p>Loading...</p>}
         <Counter 
           count={this.state.count}
           onHitClick={this.onHitClick}
